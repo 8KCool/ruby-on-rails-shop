eefeb7cc9656d91345401c17d986de4807c4df07
Dmitry Getman <dgetman94@gmail.com>
2016-10-06 17:49:05 +0700

    session key replaced, min-width: 1180px

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '072126b970e93e2b7468b6f0b4f071d0ccfb375dce47400f1757cb8f3343e203e0671c73071a817f843c72a953c8f1dd67d6c037d5eb2fc297fa2ba25b782db8'
+Rails.application.config.session_store :cookie_store, key: '_FrontLearn_session'
