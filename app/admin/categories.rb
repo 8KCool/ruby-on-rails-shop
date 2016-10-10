4362acc2f0e16499de9b861159ce246ecb9e75e6
Dmitry Getman <dgetman94@gmail.com>
2016-10-10 15:24:02 +0700

    Created base functional for Category model

---
+++ app/admin/categories.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register Category do
+  permit_params :name, :hided
+
+  ### Disable some actions
+  actions :all, except: [:show]
+
+  ### Index as table
+  index download_links: false do
+    column :name
+    column :hided
+    column :created_at
+    column :updated_at
+    actions
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140916000000) do
+ActiveRecord::Schema.define(version: 20161010075209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20140916000000) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",                       null: false
+    t.boolean  "hided",      default: false, null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "categories", ["hided"], name: "index_categories_on_hided", using: :btree
 
   create_table "nav_items", force: :cascade do |t|
     t.string   "title",                       null: false
