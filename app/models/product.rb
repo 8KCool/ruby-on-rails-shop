f7337c78045fc40cc412c8b13a2c9bfff18beb17
Dmitry Getman <dgetman94@gmail.com>
2016-10-13 10:31:51 +0700

    Added validation for price (2 decimal)

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,8 @@ class Product < ActiveRecord::Base
   belongs_to :category
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 30 }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :saleprice, numericality: { greater_than_or_equal_to: 0 }
+  validates :price, format: { with: /\A[0-9]+(\.[0-9]{1,2})?\z/ }, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :saleprice, format: { with: /\A[0-9]+(\.[0-9]{1,2})?\z/ }, numericality: { greater_than_or_equal_to: 0 }
   validates :count, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :prior, numericality: { only_integer: true }
 
