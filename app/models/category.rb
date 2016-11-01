947097fbde21dd7914120c99b813b126dc8b149c
Dmitry Getman <dgetman94@gmail.com>
2016-11-01 18:11:16 +0700

    Merged with categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ActiveRecord::Base
-  has_many :products
+  has_many :products, dependent: :destroy
   validates :name, presence: true, uniqueness: true, length: { maximum: 30 }
 end
