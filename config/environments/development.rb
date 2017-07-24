744b57cb94dd71f9c3408d7d6a009ef157b4ca6a
Dmitry Getman <dgetman94@gmail.com>
2017-07-24 11:28:40 +0000

    Add options for production mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   # Mailer settings.
-  config.action_mailer.default_url_options = { :host => 'ror-shop-example.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :letter_opener
 
   # Print deprecation notices to the Rails logger.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,14 +65,18 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Mailer settings.
-  # config.action_mailer.default_url_options = { host: "example.com" }
-  # config.action_mailer.default_options = {from: 'no-reply@example.com'}
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address:        "smtp.yandex.ru",
-  #   user_name:      "yandex_user",
-  #   password:       "user_password",
-  #   authentication: "plain" }
+  config.action_mailer.default_url_options = { host: "ror-shop-example.herokuapp.com" }
+  config.action_mailer.default_options = {from: 'no-reply@ror-shop-example.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+   address:        "smtp.sendgrid.net",
+   port:           "587",
+   user_name:      ENV['SENDGRID_USERNAME'],
+   password:       ENV['SENDGRID_PASSWORD'],
+   domain:         "heroku.com",
+   authentication: "plain",
+   enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
